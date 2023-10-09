76abe89c5472ceb21345ac993ffd4e4bea0fddef
MedantSharan <medsharanofficial@gmail.com>
2023-10-09 23:40:14 +0100

    Complete primes function

--- primes.py
+++ primes.py
@@ -3,4 +3,23 @@
 
 def primes(number_of_primes):
     list = []
+    nums = []
+    isprime = []
+    primes = []
+    for i in range(999999):
+        nums.append(i)
+        isprime.append(True)
+    for i in range(999999):
+        if i==0 or i==1:
+            continue
+        else:
+            if(isprime[i]):
+                primes.append(nums[i])
+                for j in range(2*i, 999999, i):
+                    isprime[j] = False
+            else:
+                continue
+    for i in range(number_of_primes):
+        list.append(primes[i])
+
     return list
